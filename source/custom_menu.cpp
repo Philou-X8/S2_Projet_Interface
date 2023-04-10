b80c69af6cfd7b23755ca3b792c84fb320fe4a72
Philou-X8 <pdurettesds@gmail.com>
2023-04-10 10:46:03 -0400

    created custom menu class

--- source/custom_menu.cpp
+++ source/custom_menu.cpp
@@ -1,1 +1,56 @@
 #include "custom_menu.h"
+
+
+CustomMenu::CustomMenu(UserProfile* p) : QWidget(),
+	profile(p),
+	menuLayout(nullptr),
+	activeButton(0),
+	buttonCount(0)
+{
+	activeButton = 0;
+	buttonCount = 0;
+	buttonList = new QList<CustomButton*>();
+	menuLayout = new QGridLayout(this);
+
+	setLayout(menuLayout);
+}
+CustomMenu::~CustomMenu() {
+
+}
+void CustomMenu::addButton(QString baseTex, QString hiTex) {
+	CustomButton* newButton;
+	newButton = new CustomButton(profile->getTex("home_button_continue"), profile->getTex("home_button_hi"));
+	buttonList->append(newButton);
+	menuLayout->addLayout(newButton, buttonCount, 0);
+	
+	buttonCount++;
+
+	buttonList->at(activeButton)->onSelect(); // show active button
+}
+
+void CustomMenu::onKeyEvent(char key) {
+	switch (key)
+	{
+	case 'w':
+	case 'i':
+		if (activeButton > 0) {
+			buttonList->at(activeButton)->onRelease(); // hide last selection
+			activeButton--;
+			buttonList->at(activeButton)->onSelect(); // show new selection
+		}
+		break;
+	case 's':
+	case 'k':
+		if (activeButton < buttonCount) {
+			buttonList->at(activeButton)->onRelease();
+			activeButton++;
+			buttonList->at(activeButton)->onSelect();
+		}
+		break;
+	case ' ':
+		emit clickedButton(activeButton);
+		break;
+	default:
+		break;
+	}
+}
--- source/custom_menu.h
+++ source/custom_menu.h
@@ -4,11 +4,31 @@
 #include <QWidget>
 #include <QGridLayout>
 #include <QLabel>
-
+#include "userProfile.h"
 #include "custom_button.h"
 
-class CustomMenu
+class CustomMenu : public QWidget
 {
-	//Q_OBJECT
+	Q_OBJECT
+public:
+	CustomMenu(UserProfile* p);
+	~CustomMenu();
+
+	void onKeyEvent(char key);
+
+	void addButton(QString baseTex, QString hiTex);
+	//CustomButton* getButton(int index);
+	//CustomButton* getLastButton();
+
+signals:
+	void clickedButton(int i);
+
+private:
+	UserProfile* profile;
+	QGridLayout* menuLayout;
+	QList<CustomButton*>* buttonList;
+	int activeButton;
+	int buttonCount;
+
 };
 
